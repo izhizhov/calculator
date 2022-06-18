09f9f7446b520899c1b276a594a70236b7d836ed
izhizhov <zhizhov1@gmail.com>
2022-06-18 12:54:34 +0200

    добавил обработку ошибок, изменение цвета при ошибках

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ operations = {
     '/': truediv
 }
 
+error_zero_div = 'Division by zero'
+error_undefined = 'Result is undefined'
+
 class Calculator(QMainWindow):
     def __init__(self):
         super(Calculator, self).__init__()
@@ -43,6 +46,8 @@ class Calculator(QMainWindow):
         self.ui.btn_c.clicked.connect(self.clear_all)
         self.ui.btn_ce.clicked.connect(self.clear_entry)
         self.ui.btn_dot.clicked.connect(self.add_point)
+        self.ui.btn_neg.clicked.connect(self.negate)
+        self.ui.btn_backspace.clicked.connect(self.backspace)
 
 
         #math
@@ -53,6 +58,7 @@ class Calculator(QMainWindow):
         self.ui.btn_div.clicked.connect(self.math_operation)
 
     def add_digit(self):
+        self.remove_error()
         btn = self.sender()
         digit_buttons = ('btn_0', 'btn_1', 'btn_2', 'btn_3', 'btn_4',
                          'btn_5', 'btn_6', 'btn_7', 'btn_8', 'btn_9')
@@ -63,15 +69,43 @@ class Calculator(QMainWindow):
                 self.le_entry.setText(self.le_entry.text() + btn.text())
 
     def clear_all(self) -> None:
+        self.remove_error()
         self.le_entry.setText('0')
         self.lbl_temp.clear() #не забываем убирать лишний ui.
 
     def clear_entry(self) -> None:
+        self.remove_error()
         self.le_entry.setText('0')
 
     def add_point(self) -> None:
         if '.' not in self.le_entry.text():
             self.le_entry.setText(self.le_entry.text() + '.')
+
+    def negate(self) -> None:
+        entry = self.le_entry.text()
+        if '-' not in entry:
+            if entry != '0':
+                entry = '-' + entry
+        else:
+            entry = entry[1:]
+
+        if len(entry) == self.entry_max_len + 1 and '-' in entry:
+            self.le_entry.setMaxLength(self.entry_max_len + 1)
+        else:
+            self.le_entry.setMaxLength(self.entry_max_len)
+
+        self.le_entry.setText(entry)
+    def backspace(self) -> None:
+        self.remove_error()
+        entry = self.le_entry.text()
+
+        if len(entry) != 1:
+            if len(entry) == 2 and '-' in entry:
+                self.le_entry.setText('0')
+            else:
+                self.le_entry.setText(entry[:-1])
+        else:
+            self.le_entry.setText('0')
 
     def add_temp(self) -> None:
         btn = self.sender()
@@ -104,11 +138,20 @@ class Calculator(QMainWindow):
         temp = self.lbl_temp.text()
 
         if temp:
-            result = self.remove_trailing_zeros(
-                str(operations[self.get_math_sign()](self.get_temp_num(), self.get_entry_num())))
-            self.lbl_temp.setText(temp + self.remove_trailing_zeros(entry) + ' =')
-            self.le_entry.setText(result)
-            return result
+            try:
+                result = self.remove_trailing_zeros(
+                    str(operations[self.get_math_sign()](self.get_temp_num(), self.get_entry_num())))
+                self.lbl_temp.setText(temp + self.remove_trailing_zeros(entry) + ' =')
+                self.le_entry.setText(result)
+                return result
+            except KeyError:
+                pass
+
+            except ZeroDivisionError:
+                if self.get_temp_num() == 0:
+                    self.show_error(error_undefined)
+                else:
+                    self.show_error(error_zero_div)
 
     def math_operation(self) -> None:
         temp = self.lbl_temp.text()
@@ -123,9 +166,48 @@ class Calculator(QMainWindow):
                 else:
                     self.lbl_temp.setText(temp[:-2] + f'{btn.text()} ')
             else:
-                self.lbl_temp.setText(self.calculate() + f' {btn.text()}')
+                try:
+                    self.lbl_temp.setText(self.calculate() + f' {btn.text()}')
+                except TypeError:
+                    pass
 
 
+
+    def clear_temp_if_equality(self) -> None:
+        if self.get_math_sign() == '=':
+            self.lbl_temp.clear()
+
+    def show_error(self, text: str) -> None:
+        self.le_entry.setMaxLength(len(text))
+        self.le_entry.setText(text)
+        self.disable_buttons(True)
+
+    def remove_error(self) -> None:
+        if self.le_entry.text() in (error_undefined, error_zero_div):
+            self.le_entry.setMaxLength(self.entry_max_len)
+            self.le_entry.setText('0')
+            self.disable_buttons(False)
+
+    def disable_buttons(self, disable: bool) -> None:
+        self.ui.btn_c.setDisabled(disable)
+        self.ui.btn_add.setDisabled(disable)
+        self.ui.btn_sub.setDisabled(disable)
+        self.ui.btn_mul.setDisabled(disable)
+        self.ui.btn_div.setDisabled(disable)
+        self.ui.btn_neg.setDisabled(disable)
+        self.ui.btn_dot.setDisabled(disable)
+
+        color = 'color: #888;' if disable else 'color: white;'
+        self.change_buttons_color(color)
+
+    def change_buttons_color(self, css_color: str) -> None:
+        self.ui.btn_c.setStyleSheet(css_color)
+        self.ui.btn_add.setStyleSheet(css_color)
+        self.ui.btn_sub.setStyleSheet(css_color)
+        self.ui.btn_mul.setStyleSheet(css_color)
+        self.ui.btn_div.setStyleSheet(css_color)
+        self.ui.btn_neg.setStyleSheet(css_color)
+        self.ui.btn_dot.setStyleSheet(css_color)
 
 
 if __name__ == "__main__":
